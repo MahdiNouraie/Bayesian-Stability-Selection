c419b30a2003990ec384a6dd93f38775d4b92535
Mahdi Nouraie <93427794+MahdiNouraie@users.noreply.github.com>
2024-10-22 16:08:16 +1100

    Supporting Code of Paper

---
+++ Riboflavin.R
@@ -0,0 +1,173 @@
+# This code applies Bayesian Stability Selection to the Riboflavin dataset
+# The RLARS algorithm is used to identify significant predictors
+# For getting the results similar to those reported in the manuscript,
+# you need to set b = 1000. It takes a few minutes to run.
+# For faster results, you may set b = 10.
+#################  Install and Load Necessary Libraries #################
+# Don't display warnings
+options(warn=-1)
+# Install necessary libraries if not already installed
+if(!require("hdi")){install.packages("hdi")}
+if(!require("robustHD")){install.packages("robustHD")}
+
+# Load necessary libraries
+library(hdi)  # For getting riboflavin dataset
+library(glmnet)  # For Elastic Net regression
+
+# Session Info
+#R version 4.3.2 (2023-10-31)
+#Platform: aarch64-apple-darwin20 (64-bit)
+#Running under: macOS 15.0.1
+
+#Matrix products: default
+#BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+#LAPACK: /Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/lib/libRlapack.dylib;  LAPACK version 3.11.0
+
+#attached base packages:
+#[1] parallel  stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+#[1] glmnet_4.1-8        Matrix_1.6-1.1      robustHD_0.8.1      robustbase_0.99-4-1 perry_0.3.1        
+#[6] ggplot2_3.5.1       hdi_0.1-9           scalreg_1.0.1       lars_1.3           
+
+#loaded via a namespace (and not attached):
+#[1] gtable_0.3.5      dplyr_1.1.4       compiler_4.3.2    tidyselect_1.2.1  Rcpp_1.0.13      
+#[6] splines_4.3.2     scales_1.3.0      lattice_0.22-6    R6_2.5.1          linprog_0.9-4    
+#[11] generics_0.1.3    shape_1.4.6.1     iterators_1.0.14  MASS_7.3-60       tibble_3.2.1     
+#[16] munsell_0.5.1     pillar_1.9.0      rlang_1.1.4       utf8_1.2.4        cli_3.6.3        
+#[21] withr_3.0.1       magrittr_2.0.3    foreach_1.5.2     grid_4.3.2        rstudioapi_0.16.0
+#[26] lifecycle_1.0.4   DEoptimR_1.1-3    vctrs_0.6.5       lpSolve_5.6.21    glue_1.8.0       
+#[31] codetools_0.2-20  survival_3.7-0    fansi_1.0.6       colorspace_2.1-1  tools_4.3.2      
+#[36] pkgconfig_2.0.3  
+
+#################  Bayesian Stability Selection #################
+# Load the Riboflavin dataset
+data(riboflavin)
+# Convert the dataset to a data frame
+riboflavin <- as.data.frame(cbind(Y=riboflavin$y - 1, X=riboflavin$x))
+
+# Function to update Beta distribution parameters
+# Here, success is defined as the predictor being selected and,
+# failure is defined as the predictor not being selected
+update_beta <- function(i , successes, failures, prior_alphas, prior_betas) {
+  alpha_posterior <- prior_alphas[i] + successes
+  beta_posterior <- prior_betas[i] + failures
+  return(list(alpha_posterior = alpha_posterior, beta_posterior = beta_posterior))
+}
+# Function to calculate mean and 95% credible interval of Beta distribution
+beta_stats <- function(alpha, beta) {
+  # Mean of the Beta distribution
+  mean <- alpha / (alpha + beta)
+  
+  # Lower and Upper bounds of 95% credible interval
+  lower <- qbeta(0.025, alpha, beta)
+  upper <- qbeta(0.975, alpha, beta)
+  # Return results as a list
+  return(list(mean = mean, lower = lower, upper = upper))
+}
+
+# Set seed for reproducibility
+set.seed(26)
+
+p <- ncol(riboflavin) -1 # Number of predictors
+b <- 10 # Number of subsamples
+
+# Initialize selection matrix S
+S <- matrix(data = 0, nrow = b, ncol = p)
+
+# Stability Selection
+for (i in 1:b) {
+  # Sub-sampling
+  model_data <- riboflavin[sample(1:nrow(riboflavin), nrow(riboflavin)/2, replace = FALSE), ]
+  # Prepare the response variable
+  x <- model_data[,-1]
+  y <- model_data[,1]
+  # Standardise the predictors
+  x <- scale(x)
+  # Fit the RLARS model
+  rlars_model <- rlars(x, y)
+  # Get the active predictors
+  active <- rlars_model$active
+  # Create a binary vector of significant predictors
+  significant_predictors <- rep(0, p)
+  # Set the significant predictors to 1
+  significant_predictors[active] <- 1
+  # Store significant predictors in selection matrix S
+  S[i, ] <- significant_predictors
+}
+
+# Selection frequency of each predictor
+Selection_Frequency <- colMeans(S)
+# Put names on the selection frequency elements
+names(Selection_Frequency) <- colnames(x)
+# Display predictors with selection frequency greater than 0.5
+#Selection_Frequency[Selection_Frequency > 0.5]
+
+# Put names on the selection frequency columns
+colnames(S) <- colnames(x)
+
+# 41 genes identified by Arashi et al (2021)
+genes <- c('ARGF_at', 'DNAJ_at', 'GAPB_at', 'XHLB_at', 'YACN_at',
+           'YBFI_at', 'LYSC_at', 'PKSA_at', 'PRIA_at', 'YCDH_at',
+           'YCGO_at', 'YCKE_at', 'SPOIIAA_at', 'SPOVAA_at', 'THIK_at',
+           'YCLB_at', 'YCLF_at', 'YDDH_at', 'YDDK_at', 'YEBC_at', 
+           'YFHE_r_at', 'YLXW_at', 'YMFE_at', 'YOAB_at', 'YFII_at',
+           'YFIO_at', 'YFIR_at', 'YPGA_at', 'YQJT_at', 'YQJU_at',
+           'YHDS_r_at', 'YKBA_at', 'YKVJ_at', 'YRVJ_at', 'YTGB_at',
+           'YUID_at', 'YURQ_at', 'YXLD_at', 'YXLE_at', 'YYBG_at', 'YYDA_at')
+# Get the column indices of the 41 genes
+column_index <- which(colnames(x) %in% genes)
+# determining the prior parameters for the Beta distributions
+# We assume that the for the 41 previously identified genes,
+# the answer to the first question is 50% and their perceived importance is 70%
+# For the remaining predictors, we use non-informative priors
+prior_alphas <- rep(1, p)
+prior_betas <- rep(1, p)
+prior_alphas[column_index] <- c(rep(700, length(genes)))
+prior_betas[column_index] <- c(rep(300, length(genes)))
+
+# Initialize lists to store posterior parameters for each predictor
+alpha_posterior_list <- list()
+beta_posterior_list <- list()
+# A vector to store the posterior mean of each predictor
+m <- c()
+# Two vectors to store the lower and upper bounds of the 95% credible interval for each predictor
+L <- c(); U <- c()
+# Perform Bayesian updating for each predictor
+for (j in 1:ncol(S)) {
+  # Count number of successes (1's) and failures (0's) for predictor j
+  successes <- sum(S[, j])
+  failures <- nrow(S) - successes
+  
+  # Update Beta distribution parameters
+  posterior_params <- update_beta(j , successes, failures, prior_alphas, prior_betas)
+  
+  # Store posterior parameters for predictor j
+  alpha_posterior_list[[j]] <- posterior_params$alpha_posterior
+  beta_posterior_list[[j]] <- posterior_params$beta_posterior
+  stats <- beta_stats(alpha_posterior_list[[j]], beta_posterior_list[[j]])
+  # Store the mean, lower and upper bounds of the posterior distribution for predictor j
+  m <- c(m, stats$mean)
+  L <- c(L, stats$lower)
+  U <- c(U, stats$upper)
+}
+# create a data frame to store the output
+output <- data.frame(matrix(nrow = p, ncol = 6))
+# Set names on rows corresponding to predictors
+rownames(output) <- colnames(S)
+# Set names on columns
+colnames(output) <- c('Selection Frequency', 'Posterior Mean', 
+                      'Posterior alpha', 'Posterior beta', 
+                      'Lower Bound', 'Upper Bound')
+output$`Selection Frequency` <- Selection_Frequency
+output$`Posterior Mean` <- m
+output$`Posterior alpha` <- unlist(alpha_posterior_list)
+output$`Posterior beta` <- unlist(beta_posterior_list)
+output$`Lower Bound` <- L
+output$`Upper Bound` <- U
+# Round the output to 3 decimal places
+output <- round(output, 3)
+# Order the output by posterior mean in descending order
+output <- output[order(output$`Posterior Mean`, decreasing = TRUE), ]
+# Display the top 10 predictors with the highest posterior mean
+head(output, 10)
---
+++ Synthetic_data.R
@@ -0,0 +1,113 @@
+# This code generates synthetic data and 
+#performs Bayesian Stability Selection using Elastic Net regression.
+# n_dataset denotes the number of datasets generated.
+# For getting the results similar to those reported in the manuscript,
+# you need to set n_datasets = 100. It takes a few minutes to run.
+# For faster results, you may set n_datasets = 10.
+
+#################  Install and Load Necessary Libraries #################
+# Install necessary libraries if not already installed
+if(!require("MASS")){install.packages("MASS")}
+if(!require("glmnet")){install.packages("glmnet")}
+
+# Load necessary libraries
+library(MASS)  # For multivariate Normal sampling
+library(glmnet)  # For Elastic Net regression
+
+# Session Info
+#R version 4.3.2 (2023-10-31)
+#Platform: aarch64-apple-darwin20 (64-bit)
+#Running under: macOS 15.0.1
+
+#Matrix products: default
+#BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+#LAPACK: /Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/lib/libRlapack.dylib;  LAPACK version 3.11.0
+
+#attached base packages:
+#[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+#[1] glmnet_4.1-8   Matrix_1.6-1.1 MASS_7.3-60   
+
+#loaded via a namespace (and not attached):
+#[1] compiler_4.3.2    tools_4.3.2       survival_3.7-0    rstudioapi_0.16.0 Rcpp_1.0.13      
+#[6] splines_4.3.2     codetools_0.2-20  grid_4.3.2        iterators_1.0.14  foreach_1.5.2    
+#[11] shape_1.4.6.1     lattice_0.22-6   
+
+################# Set Parameters #################
+p <- 500 # Number of predictors
+n <- 50 # Number of observations
+b <- 100 # Number of subsamples
+n_datasets <- 10 # Number of datasets
+
+# Initialize matrix for selection frequencies over datasets
+Selection_Frequency <- matrix(0, nrow = n_datasets, ncol = p)
+
+# Create the covariance matrix Σ
+Sigma <- diag(p)  # Identity matrix of size p x p
+# Specify the non-zero off-diagonal elements
+Sigma[1, 2] <- Sigma[2, 1] <- 0.8
+Sigma[3, 4] <- Sigma[4, 3] <- 0.8
+Sigma[3, 5] <- Sigma[5, 3] <- 0.8
+Sigma[4, 5] <- Sigma[5, 4] <- 0.8
+
+# Define the coefficient vector
+beta <- c(rep(0.9, 2), rep(0.7, 3), 1.5, rep(0, p - 6))  
+
+################# Data Generation and Bayesian Stability Selection #################
+# Generate synthetic datasets and loop through to calculate selection frequencies
+for (i in 1:n_datasets) {
+  set.seed(26 + i)  # Set a different seed for each dataset
+  # Sample predictors X from multivariate normal distribution N(0, Σ)
+  X <- mvrnorm(n = n, mu = rep(0, p), Sigma = Sigma)
+  
+  # Generate the response Y using the first 5 predictors and adding Gaussian noise
+  epsilon <- rnorm(n, mean = 0, sd = 2)  # Gaussian noise with variance 4 (sd = 2)
+  Y <- X %*% beta + epsilon
+  
+  # Combine predictors and response into a single data frame
+  data <- data.frame(X, Y)
+  
+  set.seed(26)  # Set seed for reproducibility
+  # Perform cross-validated Elastic Net
+  cv_elastic <- cv.glmnet(X, Y, nfolds = 10, alpha = 0.2)
+  
+  # Get the optimal lambda value
+  lambda <- cv_elastic$lambda.1se
+  
+  # Initialize matrix S for stability selection for current dataset
+  S <- matrix(data = 0, nrow = b, ncol = p)  
+  for (j in 1:b) {  # Number of subsamples
+    # Sub-sampling
+    model_data <- data[sample(1:nrow(data), nrow(data) / 2, replace = FALSE), ]
+    
+    # Prepare the predictor matrix x and response vector y for the subsample
+    x_sub <- as.matrix(model_data[, 1:ncol(model_data) - 1])
+    y_sub <- model_data$Y
+    
+    # Fit the final LASSO model with the optimal lambda value
+    elastic_model <- glmnet(x_sub, y_sub, alpha = 0.2, lambda = lambda)
+    
+    # Determine significant predictors (excluding intercept)
+    significant_predictors <- ifelse(coef(elastic_model) != 0, 1, 0)[-1]
+    
+    # Store significant predictors in matrix S
+    S[j, ] <- significant_predictors
+  }
+  
+  # Store Inclusion Probability for the dataset in the corresponding row
+  Selection_Frequency[i, ] <- colMeans(S)
+}
+
+# The nuber of times the first 6 predictors were selected
+n_j <- round(colMeans(Selection_Frequency)[1:6] * 100)
+# Using Priors for the first 6 predictors
+# Assuming the answer to the first question is 50% for the first 6 predictors and,
+# Assuming the perceived importance of the first 6 predictors is 70%
+alpha_j <- rep(70, 6)
+beta_j <- rep(30, 6)
+
+# The average selection frequency of the first 6 predictors over all datasets
+cat("Selection Frequency: ", round(colMeans(Selection_Frequency)[1:6], 3),'\n')
+# Posterior Inclusion Probability
+cat("Inclusion Probability: ", round((n_j + alpha_j) / (b + alpha_j + beta_j), 3))
