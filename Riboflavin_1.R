47fb893c7f0629bf0c49ac84714246f4228d1775
Mahdi Nouraie <93427794+MahdiNouraie@users.noreply.github.com>
2024-10-24 09:11:53 +1100

    Update Riboflavin_1.R

--- Riboflavin_1.R
+++ Riboflavin_1.R
@@ -155,5 +155,5 @@ output$`Upper Bound` <- U
 output <- round(output, 3)
 # Order the output by posterior mean in descending order
 output <- output[order(output$`Posterior Mean`, decreasing = TRUE), ]
-# Display the top 10 predictors with the highest posterior mean
+# Display the top 5 predictors with the highest posterior mean
 head(output)
