935bef33f8a2f9da1fc53676efd293258164ce86
Mahdi Nouraie <93427794+MahdiNouraie@users.noreply.github.com>
2025-02-26 15:19:18 +1100

    Update Variance_graphs.R

--- Variance_graphs.R
+++ Variance_graphs.R
@@ -12,6 +12,48 @@ library(latex2exp)
 library(reshape2)
 library(plotly)
 
+#sessionInfo()
+#R version 4.4.2 (2024-10-31)
+#Platform: x86_64-pc-linux-gnu
+#Running under: Ubuntu 20.04.6 LTS
+
+#Matrix products: default
+#BLAS:   /usr/lib/x86_64-linux-gnu/openblas-pthread/libblas.so.3 
+#LAPACK: /usr/lib/x86_64-linux-gnu/openblas-pthread/liblapack.so.3;  LAPACK version 3.9.0
+
+#locale:
+#  [1] LC_CTYPE=C.UTF-8       LC_NUMERIC=C          
+#[3] LC_TIME=C.UTF-8        LC_COLLATE=C.UTF-8    
+#[5] LC_MONETARY=C.UTF-8    LC_MESSAGES=C.UTF-8   
+#[7] LC_PAPER=C.UTF-8       LC_NAME=C             
+#[9] LC_ADDRESS=C           LC_TELEPHONE=C        
+#[11] LC_MEASUREMENT=C.UTF-8 LC_IDENTIFICATION=C   
+
+#time zone: UTC
+#tzcode source: system (glibc)
+
+#attached base packages:
+#  [1] stats     graphics  grDevices utils     datasets  methods  
+#[7] base     
+
+#other attached packages:
+#  [1] plotly_4.10.4   reshape2_1.4.4  latex2exp_0.9.6
+#[4] ggplot2_3.5.1  
+
+#loaded via a namespace (and not attached):
+#  [1] gtable_0.3.6      jsonlite_1.9.0    dplyr_1.1.4      
+#[4] compiler_4.4.2    tidyselect_1.2.1  Rcpp_1.0.14      
+#[7] stringr_1.5.1     tidyr_1.3.1       scales_1.3.0     
+#[10] fastmap_1.2.0     R6_2.6.1          plyr_1.8.9       
+#[13] generics_0.1.3    htmlwidgets_1.6.4 tibble_3.2.1     
+#[16] munsell_0.5.1     pillar_1.10.1     rlang_1.1.5      
+#[19] stringi_1.8.4     lazyeval_0.2.2    viridisLite_0.4.2
+#[22] cli_3.6.4         withr_3.0.2       magrittr_2.0.3   
+#[25] digest_0.6.37     grid_4.4.2        lifecycle_1.0.4  
+#[28] vctrs_0.6.5       glue_1.8.0        data.table_1.17.0
+#[31] colorspace_2.1-1  purrr_1.0.4       httr_1.4.7       
+#[34] tools_4.4.2       pkgconfig_2.0.3   htmltools_0.5.8.1
+
 # Define the function to return both variance values
 # V1 is variance of posterior selection probability for non-informative prior
 # V2 is variance of posterior selection probability for informative prior
